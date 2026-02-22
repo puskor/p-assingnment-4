6ffd1dbfb12eae7d6cf85aea5b4117e108960bd5
Puskor Roy <puskarkumarroy329@gmail.com>
2026-02-22 22:57:33 +0600

    change display empty

--- script/index.js
+++ script/index.js
@@ -1,4 +1,5 @@
 
+const main = document.getElementById("card_main_container");
 
 
 
@@ -17,162 +18,145 @@ document.getElementById("main_btn").addEventListener("click", function (even) {
     add_btn.classList.add("bg-blue-500", "text-white");
 })
 
+// total jobs count 
+total_jobs()
+function total_jobs() {
+    const size = main.children.length;
+    document.getElementById("total_jobs").innerText = size;
+    document.getElementById("all_jobs").innerText = size;
+    if (size == 0) {
+        add_empty_display()
+    }
+}
 
-// status change
+// delete cart 
 
+main.addEventListener("click", function (event) {
+    if (event.target.closest(".delete_btn")) {
+        const main_cart = event.target.closest(".main_cart");
+        main_cart.remove();
+        total_jobs()
+    }
+})
+
+// empty display
+function add_empty_display() {
+    const no = document.getElementById("card_main_container");
+    no.classList.add("hidden");
+    const empty_cart = document.getElementById("hide");
+    empty_cart.classList.remove("hidden");
+}
+function remove_empty_display() {
+    const empty_cart = document.getElementById("hide");
+    empty_cart.classList.add("hidden");
+    const no = document.getElementById("card_main_container");
+    no.classList.remove("hidden");
+}
+
+
+
+function show_only(type) {
+    const all_cart = document.querySelectorAll(".statuss");
+    const interview_count = document.getElementById("interview").innerText;
+    const rejected_count = document.getElementById("rejected").innerText;
+    const total_jobs = document.getElementById("total_jobs").innerText;
+    if (type == "Interview" && interview_count == 0) {
+        add_empty_display();
+    }
+    else if (type == "all" && total_jobs == 0) {
+        add_empty_display();
+    }
+    else if (type == "Rejected" && rejected_count == 0) {
+        add_empty_display();
+    }
+    else {
+        remove_empty_display();
+        all_cart.forEach(item => {
+            if (type == "all") {
+                item.parentNode.style.display = "block";
+            }
+            else if (item.innerText == type) {
+
+                item.parentNode.style.display = "block";
+            }
+            else {
+                item.parentNode.style.display = "none";
+            }
+            // console.log(item.parentNode);
+        })
+        total_item_count()
+
+    }
+
+}
+
+
+//status change
 // int_card_btn
 document.getElementById("card_main_container").addEventListener("click", function (event) {
-    if (event.target.classList.contains("int_card_btn")) {
+    const interview_button = event.target.closest(".int_card_btn");
+    if (interview_button) {
         const parent = event.target.parentNode;
         const status = parent.querySelector(".statuss");
-        status.innerText = "Applied";
-        status_color(parent);
-        count()
+        status.innerText = "Interview";
+        color(status);
+        // console.log(status.innerText);
     }
+    total_item_count()
 })
 
 // rej_card_btn
 document.getElementById("card_main_container").addEventListener("click", function (event) {
-    if (event.target.classList.contains("rej_card_btn")) {
+    const interview_button = event.target.closest(".rej_card_btn");
+    if (interview_button) {
         const parent = event.target.parentNode;
         const status = parent.querySelector(".statuss");
         status.innerText = "Rejected";
-        status_color(parent);
-        count()
+        color(status);
+        // console.log(status.innerText);
     }
+    total_item_count()
 })
 
 
-function status_color(parent) {
-    const status = parent.querySelector(".statuss");
-    if (status.innerText == "Rejected") {
-        status.classList.remove("bg-green-300");
-        status.classList.remove("bg-blue-400");
-        status.classList.add("bg-red-400");
-        status.classList.add("text-white")
-
-    }
-    else if (status.innerText == "Applied") {
+function color(status) {
+    if (status.innerText == "Interview") {
         status.classList.remove("bg-blue-100");
-        status.classList.remove("bg-red-400");
-        status.classList.add("bg-green-300");
-        status.classList.remove("text-white")
-        status.classList.add("text-black")
-
+        status.classList.remove("bg-red-400", "text-black");
+        status.classList.add("bg-green-400", "text-white");
+    }
+    else if (status.innerText == "Rejected") {
+        status.classList.remove("bg-blue-100");
+        status.classList.add("bg-red-400", "text-black");
+        status.classList.remove("bg-green-400", "text-white");
     }
     else {
-        status.classList.remove("bg-red-400");
-        status.classList.remove("bg-green-300");
         status.classList.add("bg-blue-100");
-        status.classList.remove("text-white")
-
+        status.classList.remove("bg-red-400", "text-black");
+        status.classList.remove("bg-green-400", "text-white");
     }
 }
 
 
+function total_item_count() {
+    const interview_cart = [];
+    const rejected_cart = [];
+    const all_cart = document.querySelectorAll(".statuss");
 
-const allCards = document.querySelectorAll("#card_main_container .main_cart");
-function show_only(value) {
-    const container = document.getElementById("card_main_container");
-    container.classList.remove("hidden");
-    const hide = document.getElementById("hide");
-    hide.classList.add("hidden");
-
-    allCards.forEach(item => {
-        const cart = item.querySelector(".statuss").innerText;
-        if (value == "All") {
-            item.style.display = "block"
+    all_cart.forEach(item => {
+        if (item.innerText == "Interview") {
+            interview_cart.push("1");
         }
-        else if (value == cart) {
-            item.style.display = "block"
-        }
-        else {
-            item.style.display = "none"
+        if (item.innerText == "Rejected") {
+            rejected_cart.push("1");
         }
     })
+    const interview_count = interview_cart.length;
+    const rejected_count = rejected_cart.length;
+
+    document.getElementById("interview").innerText = interview_count;
+    document.getElementById("rejected").innerText = rejected_count;
+
+    return { interview_count, rejected_count };
 }
 
-
-document.getElementById("int_btn").addEventListener("click", function () {
-    const result = count();
-    if (result.int_size == 0) {
-        hide();
-    }
-    else {
-
-        show_only('Applied')
-    }
-})
-
-document.getElementById("rej_btn").addEventListener("click", function () {
-    const result = count();
-    if (result.rej_size == 0) {
-        hide();
-    }
-    else {
-        show_only('Rejected')
-    }
-})
-
-
-
-function count() {
-    let applied = [];
-    let rejected = [];
-    allCards.forEach(item => {
-        const cart = item.querySelector(".statuss").innerText;
-        if (cart == "Applied") {
-            applied.push(cart);
-        }
-        if (cart == "Rejected") {
-            rejected.push(cart);
-        }
-    })
-
-    const interview = document.getElementById("interview");
-    const res = document.getElementById("rejected");
-    const int_size = applied.length;
-    const rej_size = rejected.length;
-    res.innerText = rej_size;
-    interview.innerText = int_size;
-
-    return { int_size, rej_size };
-
-}
-
-
-
-
-
-// all jobs and total jobs count
-total_count()
-function total_count() {
-    const size = card_main_container.children.length;
-    if (size >= 0) {
-        document.getElementById("total_jobs").innerText = size;
-        document.getElementById("all_jobs").innerText = size;
-    }
-    if(size==0) {
-        hide();
-    }
-}
-
-function hide() {
-    const container = document.getElementById("card_main_container");
-    container.classList.add("hidden");
-    const hide = document.getElementById("hide");
-    hide.classList.remove("hidden");
-}
-
-
-document.getElementById("card_main_container").addEventListener("click", function (event) {
-    if (event.target.classList.contains("delete_btn")) {
-        const parent = event.target.parentNode.parentNode.parentNode;
-        const status = parent.querySelector(".statuss");
-        status.innerText = "Not Applied";
-        status_color(parent);
-        parent.remove();
-        count();
-        total_count()
-    }
-})
